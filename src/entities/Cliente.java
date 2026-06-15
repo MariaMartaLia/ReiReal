70cf9e1f3199717bd07c0ec831179e40df1558ea
Maria Marta <liacode19@gmail.com>
2026-06-15 09:53:55 -0300

    feat: implementa entidade Cliente com validações

--- src/entities/Cliente.java
+++ src/entities/Cliente.java
@@ -3,6 +3,7 @@ package entities;
 import java.sql.Timestamp;
 import java.util.UUID;
 
+
 public class Cliente {
     private UUID id;
     private String nome;
@@ -10,41 +11,52 @@ public class Cliente {
     private Timestamp dataCriacao;
 
 
-    public Cliente(UUID id, String nome, String telefone, Timestamp dataCriacao) {
+    public Cliente(String nome, String telefone) {
         this.id = UUID.randomUUID();
         this.nome = nome;
         this.telefone = telefone;
-        this.dataCriacao = dataCriacao;
+        this.dataCriacao = new Timestamp(System.currentTimeMillis());
+
+
     }
+
     public UUID getId() {
 
         return id;
     }
+
     public String getNome() {
 
         return nome;
     }
+
     public String getTelefone() {
 
         return telefone;
     }
+
     public Timestamp getDataCriacao() {
 
         return dataCriacao;
     }
+
     public void setNome(String nome) {
 
         this.nome = nome;
     }
+
     public void setTelefone(String telefone) {
 
-        this.telefone = telefone;
-    }
-    public boolean validar(String nome, String telefone){
-        if (nome = null) {
-            return false;
-        }
-    }
+        this.telefone = telefone;}
 
-    public void
-}
+public boolean validaCliente( ){
+        if (nome.isBlank() || telefone.isBlank() ){
+        return false;
+    }
+    if (nome.matches("^[a-z A-Z]+$") && telefone.matches("^[0-9]{11}$")){
+        return true ;
+    }
+    else {
+        return false;
+    }
+}}
